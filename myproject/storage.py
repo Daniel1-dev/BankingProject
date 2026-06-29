eec30307de25da5a81e563dfa5ab5b48d204aaf0
Your Name <you@example.com>
2026-06-29 07:02:14 +0100

    Fix storage locking methods

--- myproject/storage.py
+++ myproject/storage.py
@@ -1,6 +1,5 @@
 import os
 from django.core.files.storage import FileSystemStorage
-from django.core.files import locks
 
 class NoLockFileSystemStorage(FileSystemStorage):
     """
@@ -10,14 +9,11 @@ class NoLockFileSystemStorage(FileSystemStorage):
     def _save(self, name, content):
         # We override _save to prevent calling locks.lock which fails on Android
         return super()._save(name, content)
-
-    def _open(self, name, mode='rb'):
-        return super()._open(name, mode)
     
-    def lock_file(self, fd, flags):
-        # Disable locking entirely by doing nothing
+    # Override lock and unlock methods to do absolutely nothing
+    # to avoid triggering the 'Function not implemented' error
+    def lock(self, file, flags):
         pass
-        
-    def unlock_file(self, fd):
-        # Disable unlocking entirely by doing nothing
+
+    def unlock(self, file):
         pass
