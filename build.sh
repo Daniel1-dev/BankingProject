ac9a2f03918259132cf3567fb92808c6d41f3965
Your Name <you@example.com>
2026-06-29 01:11:51 +0100

    Add requirements and build script for Render

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-p install -r requirements.txt
-
-python manage.py collectstatic --noinput
-
+#!/usr/bin/env bash
+set -o errexit
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
 python manage.py migrate
